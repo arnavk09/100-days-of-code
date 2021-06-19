91205a43dbe72fa0b91b69dbc6cd392b91f5ed94
arnavk09 <arnav02@outlook.com>
2021-06-19 14:05:08 +0530

    Day 7 (19-06-2021)

---
+++ WebDev Stuff/klakulatorapp/calculator.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser=require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}))
+const port = 3000;
+app.get("/", function (req, res) {
+    //console.log(req)
+    //res.send(`Listening to PORT ${port}`)
+    res.sendFile(__dirname+ "/index.html");
+})
+app.post("/",function (req,res) {
+    var num1,num2;
+    num1=Number(req.body.num1);
+    num2=Number(req.body.num2);
+    var submit=Number(num1+num2);
+
+
+    res.send("Answer : " + submit)
+    // console.log(req.body)
+
+    
+})
+//Listener
+app.listen(port, function () {
+    console.log("Server started at Port 3000");
+});
---
+++ WebDev Stuff/learning nodejs/server.js
@@ -0,0 +1,22 @@
+//jshint esversion : 6
+const express = require("express");
+const app = express();
+const port = 3000;
+app.get("/", function (req, res) {
+    //console.log(req)
+    res.send(`Listening to PORT ${port}`)
+})
+
+app.get("/contact", function (req, res) {
+
+    res.send("Dont contact me")
+})
+app.get("/about", function (req, res) {
+    res.send("Error 404")
+})
+
+app.listen(port, function () {
+    console.log("Server started at Port 3000");
+});
+
+
