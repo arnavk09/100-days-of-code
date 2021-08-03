5b950822b41ffc3fafb50d4a7e0afa269e80b72c
arnavk09 <arnav02@outlook.com>
2021-08-03 16:16:47 +0530

    JAVA added

---
+++ DS Algo and cpp/Java Code/DSAlgoInJava/src/AdditionClass.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class AdditionClass {
+    public static void main(String[] args) {
+        int x,y,z;
+        Scanner s= new Scanner(System.in);
+        System.out.println("Enter num1 : ");
+        x=s.nextInt();
+
+        System.out.println("Enter num2 : ");
+        y=s.nextInt();
+        z=x+y;
+        System.out.println(z);
+        System.exit(0);
+    }
+}
---
+++ DS Algo and cpp/Java Code/DSAlgoInJava/src/TestClass.java
@@ -0,0 +1,5 @@
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("Working Class, no pun intended");
+    }
+}
