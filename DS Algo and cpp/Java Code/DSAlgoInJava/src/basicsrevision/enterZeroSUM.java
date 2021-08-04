973f11489f499abbdcbb17c49761f50254f4a0ef
arnavk09 <arnav02@outlook.com>
2021-08-04 13:43:24 +0530

    A Bold New Beginning (Day 53)

--- DS Algo and cpp/Java Code/DSAlgoInJava/src/basicsrevision/enterZeroSUM.java
+++ DS Algo and cpp/Java Code/DSAlgoInJava/src/basicsrevision/enterZeroSUM.java
@@ -1,15 +1,15 @@
 package basicsrevision;
 import java.util.Scanner;
-public class enterZeroLargest {
+public class enterZeroSUM {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int sum=0;
-        System.out.println("Enter 3 num and then enter 0:");
+        System.out.println("Enter 3 num ");
         int num=sc.nextInt();
         while(num>0){
             num=sc.nextInt();
             sum=sum+num;
         }
-        System.out.println("LARGEST :"+sum);
+        System.out.println("SUM :"+sum);
     }
 }
