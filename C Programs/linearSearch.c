99a1c259bee28319b7c997f8451340fde0e98245
arnavk09 <arnav02@outlook.com>
2021-08-17 16:33:50 +0530

    Day 66

--- C Programs/linearSearch.c
+++ C Programs/linearSearch.c
@@ -5,24 +5,29 @@ int main()
     printf("Enter array size : ");
     scanf("%d", &n);
     int arr[n], element;
+    printf("Enter elements to fill in array of size %d \n ",n);
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &arr[i]);
     }
-    printf("Enter array element to find : ");
+    printf("Array : ");
+    for (int k = 0; k < n; k++)
+    {
+        printf(" %d ", arr[k]);
+    }
+    printf(" \n Enter array element to find : ");
     scanf("%d", &element);
     for (j = 0; j < n; j++)
     {
         if (arr[j] == element)
         {
-            printf("found %d at %d", element, j + 1);
+            printf("found %d at position %d \n", element, (j + 1));
             break;
         }
     }
     if (j == n)
     {
-        printf("%d isn't present in the array.\n", element);
+        printf("%d is not present in the array.\n Program is terminated.", element);
     }
-
     return 0;
 }
